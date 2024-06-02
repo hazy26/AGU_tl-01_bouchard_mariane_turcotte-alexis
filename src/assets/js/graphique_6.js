eb56edcea9aafeb30fb658ee787ba9616119ad56
Alexis <alexis@MacBookAir-de-Alexis.local>
2024-06-02 10:53:55 -0400

    update js - alexis

--- src/assets/js/graphique_6.js
+++ src/assets/js/graphique_6.js
@@ -1,21 +1,24 @@
-new RGraph.SVG.Bar({
-    id: 'graphique_6',
-    data: [[420,310],[510,270],[520,550],[545,2478]],
-    options: {
-        marginLeft: 75,
-        marginTop: 60,
-        marginInner: 10,
-        colors: ["#3e95cd", "#8e5ea2"],
-        tooltips: 'Revenue: $%{value_formatted}',
-        tooltipsCss: {
-            fontWeight: 'bold'
-        },
-        yaxisScaleUnitsPre: '$',
-        xaxisLabels: ['2020','2030','2040','2050'],
-        key: ['Frankie','Bruno'],
-        xaxisColor: '#aaa',
-        xaxisTickmarks: 0,
-        yaxisColor: '#aaa',
-        yaxisTickmarks: 0
-    }
-}).draw();
+function createGrahique6(){
+    new RGraph.SVG.Bar({
+        id: 'graphique_6',
+        data: [[420,310],[510,270],[520,550],[545,2478]],
+        options: {
+            marginLeft: 75,
+            marginTop: 60,
+            marginInner: 10,
+            colors: ["#3e95cd", "#8e5ea2"],
+            tooltips: 'Revenue: $%{value_formatted}',
+            tooltipsCss: {
+                fontWeight: 'bold'
+            },
+            yaxisScaleUnitsPre: '$',
+            xaxisLabels: ['2020','2030','2040','2050'],
+            key: ['Frankie','Bruno'],
+            xaxisColor: '#aaa',
+            xaxisTickmarks: 0,
+            yaxisColor: '#aaa',
+            yaxisTickmarks: 0
+        }
+    }).draw();
+}
+createGrahique6();
